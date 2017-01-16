ba586ebf5381592098bbba0be7e32a4794dd2f38
tmirwin <tmthy_irwin@yahoo.com>
2017-01-15 20:35:00 -0800

    Signed-off-by: tmirwin <tmthy_irwin@yahoo.com>

---
+++ Homework/Assingment_3/Gaddis_8thED_Chap4_Prob12_SoftwareSales/main.cpp
@@ -0,0 +1,68 @@
+/* 
+  File:   main.cpp
+  Author: Timothy Irwin
+  Created on January 15, 2017, 7:40 PM
+  Purpose:  A software company sells a package that retails for $99. Quantity
+            Quantity discounts are given according to the following table:
+                    10-19   20%
+                    20-49   30%
+                    50-99   40%
+                    100+    50%
+            Write a program that asks for the number of units sold and computes
+            the total cost of the purchase. (input validation: Make sure the 
+            number of units is greater than 0.)
+ */
+
+//System Libraries
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+float const Unit_Cost = 99.00; 
+
+//Function Prototypes
+
+//Executable code begins here!!!
+int main(int argc, char** argv) {
+    //Declare Variables
+    int   Number_Purchased;
+    float Cost_Units_Purchased,
+          Total_Cost;
+          
+    
+    //Input values
+    cout << "This program informs you of the total cost of your software\n"
+            "purchase after qualifying discounts (based on number of units\n"
+            "purchased) are applied.\n\n";
+    cout << "Enter the number of units you will purchase: ";
+    cin  >> Number_Purchased;
+    
+    //Process by mapping inputs to outputs
+    Cost_Units_Purchased = Number_Purchased * Unit_Cost;
+    
+    //Output values
+    cout << setprecision(2) << fixed;
+    if (Number_Purchased < 1)
+        cout << "You must purchase a minimum of 1 unit. Please restart the\n"
+                "program to make a purchase.";
+        else if (Number_Purchased < 10)
+            cout << "Total cost is: $" << Cost_Units_Purchased;
+        else if (Number_Purchased >= 10 && Number_Purchased <= 19)
+            cout << "Total cost is: $"  
+                 << (Cost_Units_Purchased -= (Cost_Units_Purchased * 0.20));
+        else if (Number_Purchased >= 20 && Number_Purchased <= 49)
+            cout << "Total cost is: $"  
+                 << (Cost_Units_Purchased -= (Cost_Units_Purchased * 0.30));
+        else if (Number_Purchased >= 50 && Number_Purchased <= 99)
+            cout << "Total cost is: $" << 
+                    (Cost_Units_Purchased -= (Cost_Units_Purchased * 0.40));
+        else if (Number_Purchased >= 100)
+            cout << "Total cost is: $" << 
+                    (Cost_Units_Purchased -= (Cost_Units_Purchased * 0.50));
+    
+    //Exit stage right!
+    return 0;
+}
